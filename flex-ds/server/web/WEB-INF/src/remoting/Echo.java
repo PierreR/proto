0766c9302327802c89a2afca8c07ed6fbed7d58f
Pierre Radermecker <pierre@vanillia-laptop.localdomain>
2009-11-16 16:31:11 +0100

    first init - Flex, DataGrid, BlazeDS - One project, two modules (client, server), one web facet in server,  no Hibernate

---
+++ flex-ds/server/web/WEB-INF/src/remoting/Echo.java
@@ -0,0 +1,8 @@
+package remoting;
+
+
+public class Echo {
+    public String echo(String txt) {
+        return "The Echo DS server says 'I receive " + txt + "' from you";
+    }
+}
---
+++ flex-ds/server/web/WEB-INF/src/utils/DAOException.java
@@ -0,0 +1,10 @@
+package utils;
+
+/**
+ * Date: Nov 16, 2009
+ */
+public class DAOException extends RuntimeException {
+    public DAOException(Throwable cause) {
+        super(cause);
+    }
+}
